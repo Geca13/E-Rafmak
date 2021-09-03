4b51a575d441d8dfb796653190e192fcc13b7772
Geca13 <karafiloski84@gmail.com>
2021-09-03 08:48:08 +0200

    adding homepage carusel

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -26,13 +26,58 @@ jsArrow.onclick = function() {
  navLinks.classList.toggle("show3");
 }
 
-if (typeof(Storage) !== "undefined") {
-  // Store
-  localStorage.setItem("user", "Smith");
-  // Retrieve
-  document.getElementById("user").innerHTML = localStorage.getItem("lastname");
-} else {
-  document.getElementById("user").innerHTML = "Sorry, your browser does not support Web Storage...";
-}
+ var slides = document.querySelectorAll('.slide');
+    var btns = document.querySelectorAll('.btn');
+    let currentSlide = 1;
+
+    // Javascript for image slider manual navigation
+    var manualNav = function(manual){
+      slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+      });
+
+      slides[manual].classList.add('active');
+      btns[manual].classList.add('active');
+    }
+
+    btns.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+      });
+    });
+
+    // Javascript for image slider autoplay navigation
+    var repeat = function(activeClass){
+      let active = document.getElementsByClassName('active');
+      let i = 1;
+
+      var repeater = () => {
+        setTimeout(function(){
+          [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove('active');
+          });
+
+        slides[i].classList.add('active');
+        btns[i].classList.add('active');
+        i++;
+
+        if(slides.length == i){
+          i = 0;
+        }
+        if(i >= slides.length){
+          return;
+        }
+        repeater();
+      }, 10000);
+      }
+      repeater();
+    }
+    repeat();
+
 
 
